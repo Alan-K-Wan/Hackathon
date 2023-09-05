601900fe270dad1ce04ad0eb98c8f719bbfc810a
Alan Wan <23072152@student.uwa.edu.au>
2023-09-05 19:40:25 +0800

    passed chat text into python function

--- askwesly/authapp/urls.py
+++ askwesly/authapp/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('secret/', views.secret_page, name='secret_page'),
+    path('process_input/', views.process_input, name='process_input'),  # Add this line
 ]
--- askwesly/authapp/views.py
+++ askwesly/authapp/views.py
@@ -2,6 +2,7 @@
 
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 
 @login_required
 def secret_page(request):
@@ -11,6 +12,19 @@ def secret_page(request):
     # Pass the user's name to the template
     context = {'user_name': user_name}
     return render(request, 'authapp/secret_page.html', context)
+
+@login_required
+def process_input(request):
+    if request.method == 'POST':
+        input_text = request.POST.get('input_text', '')
+
+        # Process the input (you can replace this with your actual processing logic)
+        processed_output = "You entered: " + input_text  # Modify this line with your processing logic
+
+        # Return the processed result as JSON
+        return JsonResponse({'result': processed_output})
+
+    return JsonResponse({'error': 'Invalid request method'})
 
     
 
